4fb854bec18be5a06107cf6b768b379ecb42c7e1
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-21 23:38:06 +0200

    Delete comments

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/controllers/HelloController.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/controllers/HelloController.java
@@ -1,49 +1,3 @@
-// // package com.example.taskflow.controllers;
-
-// // import org.springframework.web.bind.annotation.GetMapping;
-// // import org.springframework.web.bind.annotation.RequestMapping;
-// // import org.springframework.web.bind.annotation.RestController;
-
-// // @RestController
-// // @RequestMapping("/api")
-// // public class HelloWorldController {
-
-// //     @GetMapping("/hello")
-// //     public String sayHello() {
-// //         return "FUNGE";
-// //     }
-// // }
-
-// package com.example.taskflow.controllers;
-
-// import com.example.taskflow.models.Message;
-// import com.example.taskflow.repositories.MessageRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// @RequestMapping("/api")
-// public class HelloWorldController {
-
-//     @Autowired
-//     private MessageRepository greetingRepository;
-
-//     @GetMapping("/hello")
-//     public String sayHello() {
-//         // Crea un documento di esempio
-//         Message m = new Message("Pr nweuhfuon");
-        
-        
-//         // Salva il documento nel database
-//         greetingRepository.save(m);
-        
-//         // Restituisce una risposta
-//         return "Documento inserito con messaggio: " + m.getMessage();
-//     }
-// }
-
 package com.example.taskflow.controllers;
 
 import com.example.taskflow.models.Message;
@@ -64,7 +18,7 @@ public class HelloController {
     @PostMapping("/hello")
     public Map<String, String> sayHello(@RequestBody Map<String, String> requestBody) {
         // Estrai il messaggio dal corpo della richiesta
-        String messageContent = requestBody.get("message");
+        String messageContent = requestBody.get("Message");
 
         // Crea un documento con il messaggio ricevuto
         Message m = new Message(messageContent);
