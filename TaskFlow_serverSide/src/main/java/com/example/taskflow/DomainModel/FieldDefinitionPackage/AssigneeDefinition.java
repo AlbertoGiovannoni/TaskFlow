3db1ea9c95e6d1d9944db31692289fad4f109c86
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-24 13:13:30 +0200

    Agggiunti commenti e considerazioni alla classe AssigneeDefinition

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldDefinitionPackage/AssigneeDefinition.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldDefinitionPackage/AssigneeDefinition.java
@@ -7,11 +7,28 @@ import com.example.taskflow.DomainModel.User;
 
 class AssigneeDefinition extends FieldDefinition {
 
+    /*
+        FIXME: forse qua dobbiamo avere una reference ad organization?
+               Quando vado a costruire un nuovo FieldDefinition almeno 
+               non avrei bisogno di creare tutti gli oggetti
+               di tipo User?
+               Inoltre se aggiungo users duplicati questa cosa andrebbe gestita
+               qua se metto una reference diretta agli users mentre se metto
+               reference a organization posso gestirla in quella classe
+               (che secondo me ha senso perché l'organizzazione ha la
+               responsabilità di tenere gli utenti come un 'set')
+    */ 
     @DBRef
-    private ArrayList<User> possibleAssegneeUsers;      //TODO implementa lista di user
+    private ArrayList<User> possibleAssegneeUsers;
 
     public AssigneeDefinition(String nome, FieldType type) {
         super(nome, type);
+        this.possibleAssegneeUsers = new ArrayList<>();
+    }
+
+    public AssigneeDefinition(String nome, FieldType type, ArrayList<User> users) {
+        super(nome, type);
+        this.possibleAssegneeUsers = users;
     }
 
     @Override
@@ -19,8 +36,12 @@ class AssigneeDefinition extends FieldDefinition {
         //TODO
     }
 
+    //FIXME: se si cambia la reference ad Organization questi devono essere rimossi
+
     public void addUser(User user){
-        this.possibleAssegneeUsers.add(user);
+        if (!this.possibleAssegneeUsers.contains(user)){
+            this.possibleAssegneeUsers.add(user);
+        }
     }
 
     public void addUsers(ArrayList<User> users){
