a4d6050dc98e0f3d768cab76681aff6ade52a40b
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-24 13:02:46 +0200

    Aggiunta reference a database nella classe AssigneeDefinition. Aggiunto metodo per la generazione di alcuni users per testing nella classe di test FieldDefinitionTest

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldDefinitionPackage/AssigneeDefinition.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldDefinitionPackage/AssigneeDefinition.java
@@ -1,10 +1,13 @@
 package com.example.taskflow.DomainModel.FieldDefinitionPackage;
 import java.util.ArrayList;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
 import com.example.taskflow.DomainModel.User;
 
 class AssigneeDefinition extends FieldDefinition {
 
+    @DBRef
     private ArrayList<User> possibleAssegneeUsers;      //TODO implementa lista di user
 
     public AssigneeDefinition(String nome, FieldType type) {
@@ -14,5 +17,13 @@ class AssigneeDefinition extends FieldDefinition {
     @Override
     public void validateValue() {
         //TODO
+    }
+
+    public void addUser(User user){
+        this.possibleAssegneeUsers.add(user);
+    }
+
+    public void addUsers(ArrayList<User> users){
+        this.possibleAssegneeUsers.addAll(users);
     }
 }
--- TaskFlow_serverSide/src/test/java/com/example/taskflow/FieldDefinitionTest.java
+++ TaskFlow_serverSide/src/test/java/com/example/taskflow/FieldDefinitionTest.java
@@ -1,14 +1,28 @@
 package com.example.taskflow;
+import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.DomainModel.UserInfo;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinitionBuilder;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
+
+import net.bytebuddy.utility.RandomString;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import com.example.taskflow.DAOs.FieldDefinitionDAO;
+import com.example.taskflow.DAOs.UserDAO;
+import com.example.taskflow.DAOs.UserInfoDAO;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @DataMongoTest
 @ActiveProfiles("test")
@@ -16,6 +30,36 @@ public class FieldDefinitionTest {
 
     @Autowired
     FieldDefinitionDAO fieldDefinitionDAO;
+    @Autowired
+    UserDAO userDAO;
+    @Autowired
+    UserInfoDAO userInfoDAO;
+
+    @Autowired
+    MongoTemplate template;
+
+    ArrayList<User> someUsers;
+
+    @BeforeEach
+    public void setupDatabase(){
+        if (template.collectionExists("fieldDefinition")){
+            template.dropCollection("fieldDefinition");
+        }
+
+        for (int i = 0; i < 5; i++){
+            this.addRandomUserToDatabase();
+        }
+
+        this.someUsers = this.userDAO.findAll().stream().collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private void addRandomUserToDatabase(){
+        UserInfo info = new UserInfo(RandomString.make(10), RandomString.make(10), RandomString.make(10));
+        this.userInfoDAO.save(info);
+
+        User user = new User(info);
+        this.userDAO.save(user);
+    }
 
     @Test
     public void testInsertAndFindFieldDefinition() {
