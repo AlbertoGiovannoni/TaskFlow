b7b119d875a2eab8471ca55e1de4c3dc7d1f4d2f
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-29 17:20:29 +0200

    Aggiornato single selections

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/SingleSelection.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/SingleSelection.java
@@ -6,29 +6,87 @@ import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 
 public class SingleSelection extends Field{
 
-    private ArrayList<String> value;
+    private ArrayList<String> values;
     private UUID uuid;
 
     // costruttore di default
     public SingleSelection(){
     }
 
-    public SingleSelection(FieldDefinition fieldDefinition, ArrayList<String> value) {
+    public SingleSelection(FieldDefinition fieldDefinition, ArrayList<String> values) {
         super(fieldDefinition);
 
-        this.value = value;
+        this.values = values;
         this.uuid = UUID.randomUUID();
     }
 
-    public ArrayList<String> getValue() {
-        return value;
+    @Override
+    public ArrayList<?> getValues() {
+        return this.values;
     }
 
-    public void setValue(ArrayList<String> value) {
-        this.value = value;
+    @Override
+    public void removeValue(Object value) {
+        if (value != null){
+            if (value instanceof String){
+                if (this.values.contains((String)value)){
+                    this.values.remove((String)value);
+                }
+            }
+        }
     }
 
+    @Override
+    public void removeValues(ArrayList<?> values) {
+        for (Object value : values){
+            this.removeValue(value);
+        }
+    }
+
+    @Override
+    public void reset() {
+        this.values.clear();
+    }
+
+    @Override
+    public void addValue(Object value) {
+        if (value != null){
+            if (value instanceof String){
+                if (!this.values.contains((String)value)){
+                    this.values.add((String)value);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void addValues(ArrayList<?> values) {
+        for (Object value : values){
+            this.addValue(value);
+        }
+    }
+
+    @Override
+    public void setValue(Object value) {
+        this.reset();
+        this.addValue(value);
+    }
+
+    @Override
+    public void setValues(ArrayList<?> values) {
+        this.reset();
+        this.addValues(values);
+    }
+
+    @Override
     public UUID getUuid() {
-        return uuid;
+        return this.uuid;
+    }
+
+    @Override
+    public Object getValue() {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
     }
 }
