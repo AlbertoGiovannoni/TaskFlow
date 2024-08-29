7227c73f00a9676cb2daec5f3b7ceb70830727d8
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2024-08-29 17:35:52 +0200

    Creato UserTest

---
+++ TaskFlow_serverSide/src/test/java/com/example/taskflow/UserTest.java
@@ -0,0 +1,90 @@
+package com.example.taskflow;
+import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.DomainModel.UserInfo;
+import net.bytebuddy.utility.RandomString;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.example.taskflow.DAOs.FieldDefinitionDAO;
+import com.example.taskflow.DAOs.UserDAO;
+import com.example.taskflow.DAOs.UserInfoDAO;
+import java.util.ArrayList;
+
+@DataMongoTest
+@ActiveProfiles("test")
+public class UserTest {
+
+    @Autowired
+    FieldDefinitionDAO fieldDefinitionDAO;
+    @Autowired
+    UserDAO userDAO;
+    @Autowired
+    UserInfoDAO userInfoDAO;
+
+    @Autowired
+    MongoTemplate template;
+
+    ArrayList<User> someUsers;
+    ArrayList<String> someSingleSelections;
+
+    @BeforeEach
+    public void setupDatabase(){
+        if (template.collectionExists("fieldDefinition")){
+            template.dropCollection("fieldDefinition");
+        }
+        if (template.collectionExists("user")){
+            template.dropCollection("user");
+        }
+        if (template.collectionExists("userInfo")){
+            template.dropCollection("userInfo");
+        }
+    }
+
+    @Test
+    public void testInsertAndFindUser() {
+
+        // creazione e inserimento attività
+        UserInfo userinfo = new UserInfo(RandomString.make(10), RandomString.make(10));
+        userInfoDAO.save(userinfo);
+        
+        User user = new User(userinfo, RandomString.make(10));
+        user = userDAO.save(user);
+
+        User found = userDAO.findById(user.getId()).orElse(null);
+        assertNotNull(found);
+        assertEquals(user.getUsername(), found.getUsername());
+        assertEquals(user.getUserInfo().getEmail(), found.getUserInfo().getEmail());
+        assertEquals(user.getUserInfo().getPassword(), found.getUserInfo().getPassword());
+        assertEquals(user.getUserInfo().getUuid(), found.getUserInfo().getUuid());
+        assertEquals(user.getUuid(), found.getUuid());
+    }
+
+    @Test
+    public void testMondifyUser() {
+        UserInfo userinfo = new UserInfo(RandomString.make(10), RandomString.make(10));
+        userInfoDAO.save(userinfo);
+
+        User user = new User(userinfo, RandomString.make(10));
+        user = userDAO.save(user);
+
+        user.setUserInfo(userinfo);
+
+        String newUsername = RandomString.make(10);
+        user.setUsername(newUsername);
+        assertEquals(user.getUsername(), newUsername);
+
+        UserInfo newUserinfo = new UserInfo(RandomString.make(10), RandomString.make(10));
+        newUserinfo = userInfoDAO.save(newUserinfo);
+        user.setUserInfo(newUserinfo);
+        assertEquals(user.getUserInfo(), newUserinfo);
+
+        User userPushed = this.userDAO.save(user);
+        assertEquals(user.getId(), userPushed.getId());
+    }
+}
