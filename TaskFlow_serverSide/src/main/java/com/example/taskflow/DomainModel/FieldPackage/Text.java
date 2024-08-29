b3911aceedc3c2049408deddceac2ab9083f4fd1
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-29 17:26:13 +0200

    Aggiornato Text

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/Text.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/Text.java
@@ -1,5 +1,6 @@
 package com.example.taskflow.DomainModel.FieldPackage;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
@@ -20,15 +21,69 @@ public class Text extends Field{
         this.uuid = UUID.randomUUID();
     }
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
+    @Override
     public UUID getUuid() {
-        return uuid;
+        return this.uuid;
+    }
+
+    @Override
+    public Object getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void reset() {
+        this.value = "";
+    }
+
+    @Override
+    public void setValue(Object value) {
+        if (value != null){
+            if (value instanceof String){
+                this.value = (String)value;
+            }
+        }
+    }
+
+    @Override
+    public ArrayList<?> getValues() {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
+    }
+
+    @Override
+    public void removeValue(Object value) {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
+    }
+
+    @Override
+    public void removeValues(ArrayList<?> values) {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
+    }
+
+    @Override
+    public void addValue(Object value) {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
+    }
+
+    @Override
+    public void addValues(ArrayList<?> values) {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
+    }
+
+    @Override
+    public void setValues(ArrayList<?> values) {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
+                                                + " doesn't implement method " 
+                                                + this.getClass().getEnclosingMethod().toString());
     }
 }
