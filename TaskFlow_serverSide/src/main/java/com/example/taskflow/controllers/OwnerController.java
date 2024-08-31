ba8d56ef766c8246ebb519913b3ef67d69e64e31
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2024-08-31 16:18:19 +0200

    Implementata Base Auth con utenti di tipo user e owner

---
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/controllers/OwnerController.java
@@ -0,0 +1,172 @@
+package com.example.taskflow.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import com.example.taskflow.DAOs.UserDAO;
+import com.example.taskflow.DAOs.UserInfoDAO;
+import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.DomainModel.UserInfo;
+
+
+@RestController
+@RequestMapping("/api/owner")
+public class OwnerController {
+
+    @Autowired
+    UserInfoDAO userInfoDAO;
+
+    @Autowired
+    UserDAO userDAO;
+
+
+    @PostMapping("/deleteUser")
+    public ResponseEntity<Map<String, String>> deleteUser(@RequestBody Map<String, String> requestBody) {
+        // Controllo se l'utente è autenticato e ha il ruolo OWNER
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (authentication == null || !authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_OWNER"))) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Access Denied");
+            return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+        }
+
+        Map<String, String> response = new HashMap<>();
+        String userId = requestBody.get("id");
+
+        if (userId == null || userId.trim().isEmpty()) {
+            response.put("message", "Id cannot be empty");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<User> optionalUser = userDAO.findById(userId);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            UserInfo userInfo = user.getUserInfo();
+
+            userInfoDAO.delete(userInfo);
+            userDAO.delete(user);
+            response.put("message", "User removed");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            response.put("message", "User not found");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    // TODO stesse operazioni di user, le rimettiamo anche in owner?
+
+    // private ResponseEntity<Map<String, String>> validateFields(Map<String, String> requestBody) {
+    //     String email = requestBody.get("email");
+    //     String password = requestBody.get("password");
+    //     String username = requestBody.get("username");
+
+    //     Map<String, String> response = new HashMap<>();
+
+    //     if (email == null || email.trim().isEmpty()) {
+    //         response.put("message", "Email cannot be empty");
+    //         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    //     }
+
+    //     if (password == null || password.trim().isEmpty()) {
+    //         response.put("message", "Password cannot be empty");
+    //         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    //     }
+
+    //     if (username == null || username.trim().isEmpty()) {
+    //         response.put("message", "Username cannot be empty");
+    //         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    //     }
+
+    //     return null; // Nessun errore trovato
+    // }
+
+    // @PostMapping("/createUser")
+    // public ResponseEntity<Map<String, String>> createNewUser(@RequestBody Map<String, String> requestBody) {
+    //     Map<String, String> response = new HashMap<>();
+
+    //     ResponseEntity<Map<String, String>> validationResponse = validateFields(requestBody);
+
+    //     if (validationResponse != null) {
+    //         return validationResponse;
+    //     }
+
+    //     String email = requestBody.get("email");
+    //     String password = requestBody.get("password");
+    //     String username = requestBody.get("username");
+
+    //     // Controllo se l'email è già utilizzata
+    //     if (userInfoDAO.findByEmail(email).isPresent()) {
+    //         response.put("error", "Email già esistente");
+    //         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    //     }
+
+    //     // Controllo se l'username è già utilizzata
+    //     if (userDAO.findByUsername(username).isPresent()) {
+    //         response.put("error", "Username già esistente");
+    //         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    //     }
+
+    //     BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    //     String hashedPassword = passwordEncoder.encode(password);
+
+    //     UserInfo userInfo = new UserInfo(email, hashedPassword);
+    //     userInfoDAO.save(userInfo);
+    //     User user = new User(userInfo, username);
+    //     userDAO.save(user);
+
+    //     response.put("message", "User creato");
+    //     return new ResponseEntity<>(response, HttpStatus.CREATED);
+    // }
+
+    // @PostMapping("/updateUser")
+    // public ResponseEntity<Map<String, String>> updateUser(@RequestBody Map<String, String> requestBody) {
+
+    //     Map<String, String> response = new HashMap<>();
+    //     String userId = requestBody.get("id");
+
+    //     if (userId == null || userId.trim().isEmpty()) {
+    //         response.put("message", "Id cannot be empty");
+    //         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    //     }
+
+    //     Optional<User> optionalUser = userDAO.findById(requestBody.get("id"));
+
+    //     if (optionalUser.isPresent()) {
+
+    //         ResponseEntity<Map<String, String>> validationResponse = validateFields(requestBody);
+
+    //         if (validationResponse != null) {
+    //             return validationResponse;
+    //         }
+
+    //         User user = optionalUser.get();
+    //         UserInfo userInfo = user.getUserInfo();
+
+    //         userInfo.setEmail(requestBody.get("email"));
+    //         userInfo.setPassword(requestBody.get("password"));
+    //         user.setUsername(requestBody.get("username"));
+
+    //         userInfoDAO.save(userInfo);
+    //         userDAO.save(user);
+    //     } else {
+    //         response.put("message", "User not found");
+    //     }
+
+    //     response.put("message", "User aggiornato");
+    //     return new ResponseEntity<>(response, HttpStatus.CREATED);
+    // }
+
+
+
+}
