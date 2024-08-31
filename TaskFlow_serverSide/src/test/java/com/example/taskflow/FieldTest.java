52a19ec53edfadd1ffe1f97687eabc558adc9ccf
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-31 14:55:09 +0200

    Aggiunto field test

--- TaskFlow_serverSide/src/test/java/com/example/taskflow/FieldTest.java
+++ TaskFlow_serverSide/src/test/java/com/example/taskflow/FieldTest.java
@@ -2,9 +2,10 @@ package com.example.taskflow;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
 import java.util.Set;
-import java.util.random.RandomGeneratorFactory;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.example.taskflow.DAOs.FieldDAO;
 import com.example.taskflow.DAOs.FieldDefinitionDAO;
+import com.example.taskflow.DomainModel.User;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
 import com.example.taskflow.DomainModel.FieldPackage.Field;
@@ -45,7 +47,7 @@ public class FieldTest {
     }
 
     @Test
-    public void testAddingNewTextField(){
+    public void testTextField(){
         FieldDefinition fieldDefinition = this.testUtil.pushGetRandomFieldDefinitionToDatabase(FieldType.TEXT);
 
         Field field = FieldFactory.getBuilder(FieldType.TEXT)
@@ -57,10 +59,19 @@ public class FieldTest {
 
         assertEquals(field, fieldFromDB);
         assertEquals(field.getFieldDefinition(), fieldDefinition);
+
+        String randomString = RandomString.make(10);
+        fieldFromDB.setValue(randomString);
+
+        Field fieldModified = this.fieldDao.save(fieldFromDB);
+
+        assertEquals(fieldModified.getValue(), randomString);
+        assertEquals(fieldModified.getUuid(), fieldFromDB.getUuid());
+        assertEquals(field.getUuid(), fieldFromDB.getUuid());
     }
 
     @Test
-    public void testAddingNewNumberField(){
+    public void testNumberField(){
         FieldDefinition fieldDefinition = this.testUtil.pushGetRandomFieldDefinitionToDatabase(FieldType.NUMBER);
 
         Field field = FieldFactory.getBuilder(FieldType.NUMBER)
@@ -72,5 +83,81 @@ public class FieldTest {
 
         assertEquals(field, fieldFromDB);
         assertEquals(field.getFieldDefinition(), fieldDefinition);
+
+        Float randomFloat = (float)Math.random();
+        fieldFromDB.setValue(randomFloat);
+
+        Field fieldModified = this.fieldDao.save(fieldFromDB);
+
+        assertEquals(fieldModified.getValue(), randomFloat);
+        assertEquals(fieldModified.getUuid(), fieldFromDB.getUuid());
+        assertEquals(field.getUuid(), fieldFromDB.getUuid());
+    }
+
+    @Test
+    public void testSingleSelectionField(){
+        FieldDefinition fieldDefinition = this.testUtil.pushGetRandomFieldDefinitionToDatabase(FieldType.SINGLE_SELECTION);
+
+        ArrayList<String> someSelections = new ArrayList<>();
+        ArrayList<String> subsetOfSomeSelections = new ArrayList<>();
+
+        String randomString = RandomString.make(10);
+
+        for (int i = 0; i < 10; i++){
+            someSelections.add(randomString);
+            if (i % 2 == 0){
+                subsetOfSomeSelections.add(randomString);
+            }
+        }
+
+        fieldDefinition.addMultipleEntry(someSelections);
+        this.fieldDefinitionDao.save(fieldDefinition);
+
+        Field field = FieldFactory.getBuilder(FieldType.SINGLE_SELECTION)
+                                .addFieldDefinition(fieldDefinition)
+                                .addParameter(someSelections.get(1))
+                                .build();
+
+        Field fieldFromDB = this.fieldDao.save(field);
+
+        assertEquals(field, fieldFromDB);
+        assertEquals(field.getFieldDefinition(), fieldDefinition);
+
+        fieldFromDB.setValues(subsetOfSomeSelections);
+
+        assertEquals(field, fieldFromDB);
+        assertEquals(fieldFromDB.getFieldDefinition(), fieldDefinition);
+
+        int i = 0;
+        for (Object value : fieldFromDB.getValues()){
+            assertEquals((String)value, subsetOfSomeSelections.get(i));
+            i++;
+        }
+
+        assertThrows(IllegalArgumentException.class, ()->{fieldFromDB.addValue("a string not accepted");});
+
+        assertEquals(field, this.fieldDao.save(fieldFromDB));
+    }
+
+    @Test
+    public void testAssigneeField(){
+        FieldDefinition fieldDefinition = this.testUtil.pushGetRandomFieldDefinitionToDatabase(FieldType.ASSIGNEE);
+
+        ArrayList<User> someUsers = this.testUtil.addGetMultipleRandomUserToDatabase(5);
+
+        fieldDefinition.addMultipleEntry(someUsers);
+        this.fieldDefinitionDao.save(fieldDefinition);
+
+        Field field = FieldFactory.getBuilder(FieldType.ASSIGNEE)
+                                .addFieldDefinition(fieldDefinition)
+                                .addParameter(someUsers.get(1))
+                                .build();
+
+        Field fieldFromDB = this.fieldDao.save(field);
+
+        assertEquals(field, fieldFromDB);
+        assertEquals(field.getFieldDefinition(), fieldDefinition);
+
+
     }
 }
