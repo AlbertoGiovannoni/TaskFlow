d2fbfcc6007fcbe7f4ceefe2528d7e9be629898e
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-31 14:54:06 +0200

    Aggiunto TestUtil

--- TaskFlow_serverSide/src/test/java/com/example/taskflow/TestUtil.java
+++ TaskFlow_serverSide/src/test/java/com/example/taskflow/TestUtil.java
@@ -1,5 +1,6 @@
 package com.example.taskflow;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 
 import com.example.taskflow.DAOs.FieldDefinitionDAO;
+import com.example.taskflow.DAOs.UserDAO;
+import com.example.taskflow.DAOs.UserInfoDAO;
+import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.DomainModel.UserInfo;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinitionFactoryPackage.FieldDefinitionFactory;
@@ -23,9 +28,13 @@ import net.bytebuddy.utility.RandomString;
 public class TestUtil {
 
     @Autowired
-    MongoTemplate template;
+    private MongoTemplate template;
     @Autowired
-    FieldDefinitionDAO fieldDefinitionDAO;
+    private FieldDefinitionDAO fieldDefinitionDAO;
+    @Autowired
+    private UserInfoDAO userInfoDAO;
+    @Autowired
+    private UserDAO userDAO;
 
     public void cleanDatabase(){
         Set<String> allCollections = this.template.getCollectionNames();
@@ -41,5 +50,23 @@ public class TestUtil {
                                     .build();
 
         return this.fieldDefinitionDAO.save(fieldDefinition);
+    }
+
+    public User addGetRandomUserToDatabase(){
+        UserInfo info = new UserInfo(RandomString.make(10), RandomString.make(10));
+        this.userInfoDAO.save(info);
+
+        User user = new User(info, RandomString.make(10));
+        return this.userDAO.save(user);
+    }
+
+    public ArrayList<User> addGetMultipleRandomUserToDatabase(int n){
+        ArrayList<User> users = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            users.add(this.addGetRandomUserToDatabase());
+        }
+
+        return users;
     }
 }
