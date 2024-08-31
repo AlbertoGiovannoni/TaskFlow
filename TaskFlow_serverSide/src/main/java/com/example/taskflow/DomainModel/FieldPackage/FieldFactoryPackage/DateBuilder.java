f51a6a05d253cbe8615028c04ff340f40cd2459e
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-08-31 17:00:34 +0200

    Rimosso metodo self e cambiata reference a DateData

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/FieldFactoryPackage/DateBuilder.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/FieldFactoryPackage/DateBuilder.java
@@ -1,15 +1,42 @@
 package com.example.taskflow.DomainModel.FieldPackage.FieldFactoryPackage;
 import java.util.ArrayList;
 import com.example.taskflow.DomainModel.FieldPackage.Date;
-import com.example.taskflow.DomainModel.FieldPackage.DateInfo;
+import com.example.taskflow.DomainModel.FieldPackage.DateData;
 import com.example.taskflow.DomainModel.FieldPackage.Field;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
 
 public class DateBuilder extends FieldBuilder{
-    private DateInfo dateInfo;
+    private DateData dateData;
 
     DateBuilder(FieldType type) {
         super(type);
+    }
+
+    @Override
+    public FieldBuilder addParameter(Object value){
+        if (value != null){
+            if (value instanceof DateData){
+                this.dateData = (DateData)value;                   
+            }
+            else{
+                throw new IllegalArgumentException("value is not of type DateData:" + value);
+            }
+        }
+        else{
+            throw new IllegalArgumentException("value is null:" + value);
+        }
+        return this;
+    }
+
+    @Override
+    public Field build() {
+        if (this.dateData == null){
+            throw new IllegalAccessError("dateData is null: " + this.dateData);
+        }
+        else if (this.fieldDefinition == null){
+            throw new IllegalAccessError("fieldDefinition is null: " + this.fieldDefinition);
+        }
+        return new Date(this.fieldDefinition, this.dateData);
     }
 
     @Override
@@ -17,39 +44,5 @@ public class DateBuilder extends FieldBuilder{
         throw new UnsupportedOperationException(this.getClass().getSimpleName() 
                                                 + "doesn't implement method " 
                                                 + this.getClass().getEnclosingMethod().toString());
-    }
-
-    @Override
-    public FieldBuilder addParameter(Object value){
-        if (value != null){
-            if (value instanceof DateInfo){
-                this.dateInfo = (DateInfo)value;                   
-            }
-            else{
-                throw new IllegalArgumentException("value is not of type DateInfo:" + value);
-            }
-        }
-        else{
-            throw new IllegalArgumentException("value is null:" + value)
-        }
-        return this;
-    }
-
-    @Override
-    protected DateBuilder self() {
-        return this;
-    }
-
-    @Override
-    public Field build() {
-        if (this.dateInfo == null){
-            throw new IllegalAccessError("date is null");
-        }
-        if (this.dateInfo.getNotification() == null){
-            return new Date(this.fieldDefinition, dateInfo.getValue());
-        }
-        else{
-            return new Date(this.fieldDefinition, dateInfo);
-        }
     }
 }
