bac9a26def7b57f90b7b124d780c39af2576f2fd
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2024-08-31 15:31:57 +0200

    Aggiunto controller user per creazione e modifica

---
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.taskflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.service.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // TODO implementa baseAuth o Token
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Disabilita CSRF per semplicita durante lo sviluppo
+            .authorizeHttpRequests(requests -> requests
+                    .requestMatchers("/api/user/**").permitAll() // Permetti l'accesso a tutti gli endpoint API
+                    .anyRequest().permitAll())
+            .httpBasic(basic -> basic.disable()); // Disabilita l'autenticazione di base
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
