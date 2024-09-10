332e5f25d9d6de228e4c1b97f9cfca0e8eb9c8b6
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2024-09-10 11:25:26 +0200

    Aggiunto nel userController createActivity e updateActivity

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/FieldFactoryPackage/NumberBuilder.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DomainModel/FieldPackage/FieldFactoryPackage/NumberBuilder.java
@@ -1,11 +1,12 @@
 package com.example.taskflow.DomainModel.FieldPackage.FieldFactoryPackage;
+
 import com.example.taskflow.DomainModel.FieldPackage.Field;
 import com.example.taskflow.DomainModel.FieldPackage.Number;
 import com.example.taskflow.DomainModel.User;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
 import java.util.ArrayList;
 
-public class NumberBuilder extends FieldBuilder{
+public class NumberBuilder extends FieldBuilder {
     private Float value;
 
     NumberBuilder(FieldType type) {
@@ -14,24 +15,26 @@ public class NumberBuilder extends FieldBuilder{
 
     @Override
     public FieldBuilder addParameters(ArrayList<?> values) {
-        throw new UnsupportedOperationException(this.getClass().getSimpleName() 
-                                                + "doesn't implement method " 
-                                                + this.getClass().getEnclosingMethod().toString());
+        return this.addParameter(values.get(0));
     }
 
     @Override
-    public NumberBuilder addParameter(Object value){
-        if (value != null){
-            if (value instanceof User){
-                this.value = (Float)value;
+    public NumberBuilder addParameter(Object value) {
+        if (value != null) {
+            if (value instanceof Float) {
+                this.value = (Float) value;
+            } else {
+                throw new IllegalArgumentException("value is not of type DateData:" + value);
             }
+        } else {
+            throw new IllegalArgumentException("value is null:" + value);
         }
         return this;
     }
 
     @Override
     public Field build() {
-        if (this.value != null){
+        if (this.value == null) {
             throw new IllegalAccessError("value is null");
         }
         return new Number(this.fieldDefinition, this.value);
