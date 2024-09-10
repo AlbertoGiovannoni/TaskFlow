a7e72f9ab382965963b9b757b726531c0a3b46f6
danimorgan00 <daniele.morganti07@gmail.com>
2024-09-10 10:39:22 +0200

    Aggiunti include in acticityTest

--- TaskFlow_serverSide/src/test/java/com/example/taskflow/ActivityTest.java
+++ TaskFlow_serverSide/src/test/java/com/example/taskflow/ActivityTest.java
@@ -2,16 +2,38 @@ package com.example.taskflow;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.example.taskflow.DAOs.ActivityDAO;
+import com.example.taskflow.DAOs.FieldDAO;
+import com.example.taskflow.DAOs.FieldDefinitionDAO;
+import com.example.taskflow.DAOs.NotificationDAO;
+import com.example.taskflow.DAOs.UserDAO;
 import com.example.taskflow.DomainModel.Activity;
+import com.example.taskflow.DomainModel.Notification;
+import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
+import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
+import com.example.taskflow.DomainModel.FieldPackage.Date;
+import com.example.taskflow.DomainModel.FieldPackage.DateData;
+import com.example.taskflow.DomainModel.FieldPackage.Field;
+import com.example.taskflow.DomainModel.FieldPackage.FieldFactoryPackage.FieldFactory;
+
 
 import net.bytebuddy.utility.RandomString;
 
