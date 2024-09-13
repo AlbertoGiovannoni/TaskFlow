804c2a0e796af4e9e7bff223578ed92493f2a448
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-09-13 18:02:45 +0200

    Aggiunto metodo per effettuare eliminazione by FieldDefinition

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DAOs/FieldDAO.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/DAOs/FieldDAO.java
@@ -1,6 +1,9 @@
 package com.example.taskflow.DAOs;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 import com.example.taskflow.DomainModel.FieldPackage.Field;
 
 public interface FieldDAO extends MongoRepository<Field, String> {
+    public void deleteFieldByFieldDefinition(FieldDefinition fieldDefinition);
 }
