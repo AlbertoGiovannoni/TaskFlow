75df9210d4fb3f00b328ce2c4b9b721adae6d03e
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-09-15 10:48:03 +0200

    Aggiunto metodo a FieldDefinitionService

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/service/FieldDefinitionService.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/service/FieldDefinitionService.java
@@ -7,19 +7,24 @@ import org.springframework.stereotype.Service;
 
 import com.example.taskflow.DAOs.FieldDAO;
 import com.example.taskflow.DAOs.FieldDefinitionDAO;
+import com.example.taskflow.DAOs.UserDAO;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldType;
-import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinitionFactoryPackage.FieldDefinitionBuilder;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinitionFactoryPackage.FieldDefinitionFactory;
+
 
 @Service
 public class FieldDefinitionService {
     @Autowired
-    private FieldDefinitionDAO fieldDefinitionDao;
+    FieldDefinitionDAO fieldDefinitionDao;
 
     @Autowired
-    private FieldDAO fieldDao;
+    FieldDAO fieldDao;
 
+    @Autowired
+    UserDAO userDao;
+
+    //FIXME: questa operazione probabilmente va fatta direttamente nel controller
     public void delete(String id){
         FieldDefinition fieldDefinition = this.fieldDefinitionDao.findById(id).orElseThrow();
 
@@ -48,5 +53,13 @@ public class FieldDefinitionService {
                                             .build();
         this.fieldDefinitionDao.save(fieldDefinition);
         return fieldDefinition;
+    }
+
+    public FieldDefinition modifyName(String id, String newName){
+        FieldDefinition fieldDefinition = this.fieldDefinitionDao.findById(id).orElseThrow();
+
+        fieldDefinition.setName(newName);
+
+        return this.fieldDefinitionDao.save(fieldDefinition);
     }
 }
