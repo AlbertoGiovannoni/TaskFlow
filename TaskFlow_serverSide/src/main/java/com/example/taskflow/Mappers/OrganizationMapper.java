10975ec18212ccfed12a68383231163b2bd0772b
danimorgan00 <daniele.morganti07@gmail.com>
2024-09-18 18:46:30 +0200

    Implementato toDto di organizationMapper, TODO: toEntity

---
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/Mappers/OrganizationMapper.java
@@ -0,0 +1,37 @@
+package com.example.taskflow.Mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import java.util.ArrayList;
+
+import com.example.taskflow.DTOs.OrganizationDTO;
+import com.example.taskflow.DomainModel.Organization;
+import com.example.taskflow.DomainModel.Project;
+import com.example.taskflow.DomainModel.User;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public class OrganizationMapper {
+
+    OrganizationDTO toDto(Organization organization) {
+        ArrayList<String> ownersId = new ArrayList<String>();
+        ArrayList<String> membersId = new ArrayList<String>();
+        ArrayList<String> projectsId = new ArrayList<String>();
+
+        for (User owner : organization.getOwners()) {
+            ownersId.add(owner.getId());
+        }
+        for (User member : organization.getMembers()) {
+            membersId.add(member.getId());
+        }
+        for (Project project : organization.getProjects()) {
+            projectsId.add(project.getId());
+        }
+
+        OrganizationDTO organizationDTO = new OrganizationDTO(organization.getId(), organization.getName(), organization.getCreationDate(), ownersId, membersId, projectsId);
+        return organizationDTO;
+    };
+
+    Organization toEntity(OrganizationDTO organizationDto) {
+        //TODO lo fo domani
+    }
+}
