3323a9f23689e669eccbaf9411742610df219b3a
danimorgan00 <daniele.morganti07@gmail.com>
2024-09-20 16:13:15 +0200

    Scritto ProjectService

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/service/ProjectService.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/service/ProjectService.java
@@ -7,7 +7,15 @@ import org.springframework.stereotype.Service;
 
 import com.example.taskflow.DAOs.ProjectDAO;
 import com.example.taskflow.DAOs.UserDAO;
+import com.example.taskflow.DTOs.ActivityDTO;
+import com.example.taskflow.DTOs.ProjectDTO;
+import com.example.taskflow.DTOs.Field.FieldDTO;
+import com.example.taskflow.DomainModel.Activity;
 import com.example.taskflow.DomainModel.Project;
+import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
+import com.example.taskflow.DomainModel.FieldPackage.Field;
+import com.example.taskflow.Mappers.ActivityMapper;
+import com.example.taskflow.Mappers.ProjectMapper;
 
 
 @Service
@@ -19,11 +27,27 @@ public class ProjectService {
     @Autowired
     UserDAO userDao;
 
-    // public void delete(ProjectDTO projectDto){
-    //     Project project = this.projectDao.findById(projectDto.getId()).orElseThrow();
+    @Autowired
+    ProjectMapper projectMapper;
 
-    //     //TODO cascading
+    @Autowired
+    ActivityService activityService;
 
-    //     this.projectDao.delete(project);
-    // }
+    public ProjectDTO createProject(ProjectDTO projectDto){
+        Project project = projectMapper.toEntity(projectDto);
+        
+        ArrayList<Activity> activities = new ArrayList<Activity>();
+        project.setActivities(activities);
+        this.projectDao.save(project);
+
+        return projectMapper.toDto(project);
+    }
+    
+    public void deleteProject(ProjectDTO projectDto){
+        Project project = this.projectDao.findById(projectDto.getId()).orElseThrow();
+        for(Activity activity:project.getActivities()){
+            activityService.deleteActivity(activity);           //TOIMPLEMENT deletActivity
+        }
+        this.projectDao.delete(project);
+    }
 }
