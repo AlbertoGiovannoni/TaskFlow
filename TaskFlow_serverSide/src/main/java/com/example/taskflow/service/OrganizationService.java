e70220047d7eabef7981350660bf75b91ad1e58d
danimorgan00 <daniele.morganti07@gmail.com>
2024-09-20 16:35:03 +0200

    Scritto OrganizationService

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/service/OrganizationService.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/service/OrganizationService.java
@@ -1,11 +1,17 @@
 package com.example.taskflow.service;
 
 import com.example.taskflow.DAOs.OrganizationDAO;
+import com.example.taskflow.DAOs.ProjectDAO;
+import com.example.taskflow.DTOs.OrganizationDTO;
 import com.example.taskflow.DomainModel.Organization;
-import com.example.taskflow.DomainModel.User;
+import com.example.taskflow.DomainModel.Project;
+import com.example.taskflow.Mappers.OrganizationMapper;
+import com.example.taskflow.Mappers.ProjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
@@ -13,6 +19,14 @@ public class OrganizationService {
 
     @Autowired
     private OrganizationDAO organizationDAO;
+    @Autowired
+    private ProjectDAO projectDAO;
+    @Autowired
+    private OrganizationMapper organizationMapper;
+    @Autowired
+    private ProjectMapper projectMapper;
+    @Autowired
+    private ProjectService projectService;
 
     
     // Verifica se l'utente è un OWNER in una specifica organizzazione. 
@@ -26,7 +40,6 @@ public class OrganizationService {
         return false;
     }
 
-    
     // Verifica se l'utente è un USER in una specifica organizzazione.
     public boolean isUser(String organizationId, String username) {
         Optional<Organization> organization = organizationDAO.findById(organizationId);
@@ -36,5 +49,26 @@ public class OrganizationService {
                     .anyMatch(member -> member.getUsername().equals(username));
         }
         return false;
+    }
+
+    public OrganizationDTO createOrganization(OrganizationDTO organizationDTO){
+        Organization organization = organizationMapper.toEntity(organizationDTO);
+        
+        ArrayList<Project> projects = new ArrayList<Project>();
+        organization.setProjects(projects);
+        this.organizationDAO.save(organization);
+
+        return organizationMapper.toDto(organization);
+    }
+
+    
+    public void deleteOrganization(OrganizationDTO organizationDTO){
+        Organization organization = this.organizationDAO.findById(organizationDTO.getId()).orElseThrow();
+        Project project;
+        for(String projectId:organizationDTO.getProjectsId()){
+            project = this.projectDAO.findById(projectId).orElseThrow();           
+            projectService.deleteProject(projectMapper.toDto(project));
+        }
+        this.organizationDAO.delete(organization);
     }
 }
--- TaskFlow_serverSide/src/main/java/com/example/taskflow/service/ProjectService.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/service/ProjectService.java
@@ -7,14 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.example.taskflow.DAOs.ProjectDAO;
 import com.example.taskflow.DAOs.UserDAO;
-import com.example.taskflow.DTOs.ActivityDTO;
 import com.example.taskflow.DTOs.ProjectDTO;
-import com.example.taskflow.DTOs.Field.FieldDTO;
 import com.example.taskflow.DomainModel.Activity;
 import com.example.taskflow.DomainModel.Project;
-import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
-import com.example.taskflow.DomainModel.FieldPackage.Field;
-import com.example.taskflow.Mappers.ActivityMapper;
 import com.example.taskflow.Mappers.ProjectMapper;
 
 
@@ -46,7 +41,7 @@ public class ProjectService {
     public void deleteProject(ProjectDTO projectDto){
         Project project = this.projectDao.findById(projectDto.getId()).orElseThrow();
         for(Activity activity:project.getActivities()){
-            activityService.deleteActivity(activity);           //TOIMPLEMENT deletActivity
+            //activityService.deleteActivity(activity);           //PENDING: implement deleteActivity
         }
         this.projectDao.delete(project);
     }
