18badf306849562d39188dfec36408fd5ccd87e4
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-09-23 16:57:49 +0200

    Rimossi refusi di classi vecchie

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/DAOs/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskflow.DAOs;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.taskflow.DomainModel.Message;
-
-public interface MessageRepository extends MongoRepository<Message, String> {
-}
