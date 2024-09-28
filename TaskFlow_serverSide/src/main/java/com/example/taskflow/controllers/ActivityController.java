fc0b10831e46e0c471bd88429de89639596b5c63
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2024-09-28 11:42:43 +0200

    aggiunto gestore eccezioni

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/controllers/ActivityController.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/controllers/ActivityController.java
@@ -1,10 +1,13 @@
 
 package com.example.taskflow.controllers;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.taskflow.DAOs.ActivityDAO;
@@ -44,6 +47,19 @@ public class ActivityController {
     FieldServiceManager fieldServiceManager;
     @Autowired
     UserDAO userDAO;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
     @PatchMapping("/user/{userId}/myOrganization/{organizationId}/projects/{projectId}/activities/{activityId}")
     public ResponseEntity<Activity> renameActivity(@PathVariable String activityId,
