f96e27af8683721f3be4bc7dcc8412cd31f5a1aa
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2024-10-03 12:45:09 +0200

    Aggiunto metodo per l'aggiornamento del document

--- TaskFlow_serverSide/src/main/java/com/example/taskflow/service/FieldService/DocumentService.java
+++ TaskFlow_serverSide/src/main/java/com/example/taskflow/service/FieldService/DocumentService.java
@@ -1,40 +1,31 @@
 package com.example.taskflow.service.FieldService;
-import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.example.taskflow.DAOs.FieldDAO;
 import com.example.taskflow.DTOs.Field.DocumentDTO;
 import com.example.taskflow.DTOs.Field.DocumentUploadDTO;
 import com.example.taskflow.DTOs.Field.FieldDTO;
-import com.example.taskflow.DTOs.Field.TextDTO;
 import com.example.taskflow.DomainModel.FieldDefinitionPackage.FieldDefinition;
 import com.example.taskflow.DomainModel.FieldPackage.Document;
 import com.example.taskflow.DomainModel.FieldPackage.Field;
 import com.example.taskflow.DomainModel.FieldPackage.FieldFactoryPackage.DocumentBuilder;
-import com.example.taskflow.DomainModel.FieldPackage.FieldFactoryPackage.TextBuilder;
-import com.example.taskflow.Mappers.FieldMapper;
 
 @Service
 public class DocumentService extends FieldService{
 
     @Autowired
     private FieldDAO fieldDao;
-    @Autowired
-    private FieldMapper fieldMapper;
 
-    public Document pushDocument(DocumentDTO documentDto, MultipartFile file) throws IOException{
-        Document document = this.fieldMapper.toEntity(documentDto);
-        document.setContent(file.getBytes());
+    public byte[] download(String documentFieldId){
+        Document document = (Document)this.fieldDao.findById(documentFieldId).orElseThrow();
 
-        return fieldDao.save(document);
-    }
+        if (document.getContent() == null){
+            throw new IllegalArgumentException("Document is empty");
+        }
 
-    public Document getDocumentById(String id) {
-        Document document = (Document)this.fieldDao.findById(id).orElseThrow();
-        return document;
+        return document.getContent();
     }
 
     @Override
@@ -67,7 +58,30 @@ public class DocumentService extends FieldService{
 
     @Override
     public Field updateField(FieldDTO fieldDto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateField'");
+        if (!(fieldDto instanceof DocumentDTO)){
+            throw new IllegalArgumentException("Field is not a DocumentDto");
+        }
+
+        DocumentDTO documentDto = (DocumentDTO)fieldDto;
+
+        Document document = (Document)this.fieldDao.findById(documentDto.getId()).orElseThrow();
+
+        if (documentDto.getFileName() != null){
+            if (documentDto.getFileName() != ""){
+                document.setFileName(documentDto.getFileName());
+            }
+        }
+
+        if (documentDto.getFileType() != null){
+            if (documentDto.getFileType() != ""){
+                document.setFileType(documentDto.getFileType());
+            }
+        }
+
+        if (fieldDto instanceof DocumentUploadDTO){
+            document.setContent(((DocumentUploadDTO)fieldDto).getContent());
+        }
+
+        return this.fieldDao.save(document);
     }
 }
